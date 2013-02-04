a9b39e8fddccbc8972e4b57b2806531df1934738
Miroslav Rachev <miro@mirosr.net>
2013-02-04 15:31:00 +0200

    Added scope in scenario 'Showing the main menu'

--- spec/features/main_menu_spec.rb
+++ spec/features/main_menu_spec.rb
@@ -4,10 +4,12 @@ feature 'Main menu' do
   scenario 'Showing the main menu' do
     visit root_path
 
-    expect(page).to have_link('Dashboard', href: dashboard_path)
-    expect(page).to have_content 'Expenditures'
-    expect(page).to have_content 'Groups'
-    expect(page).to have_content 'Reports'
-    expect(page).to have_content 'Admin Panel'
+    within 'nav.main' do
+      expect(page).to have_link('Dashboard', href: dashboard_path)
+      expect(page).to have_content 'Expenditures'
+      expect(page).to have_content 'Groups'
+      expect(page).to have_content 'Reports'
+      expect(page).to have_content 'Admin Panel'
+    end
   end
 end
