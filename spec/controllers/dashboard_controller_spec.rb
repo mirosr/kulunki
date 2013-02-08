9e6cd1c59a7baf432dcf44870e65cca7f5df1031
Miroslav Rachev <miro@mirosr.net>
2013-02-08 19:59:14 +0200

    Add sign in redirection in dashboard controller spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe DashboardController do
   describe 'GET #show' do
+    context 'when user is not signed in' do
+      it 'redirects to sign in url' do
+        get :show
+
+        expect(response).to redirect_to signin_path
+      end
+    end
+
     it 'renders the :show template' do
       login_user build_stubbed(:user)
 
