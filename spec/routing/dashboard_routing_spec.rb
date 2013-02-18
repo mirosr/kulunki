440368f9778f523124de74471b1d43a980a5440d
Miroslav Rachev <miro@mirosr.net>
2013-02-18 23:41:34 +0200

    Prefer actual path when describing routing spec

--- spec/routing/dashboard_routing_spec.rb
+++ spec/routing/dashboard_routing_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Dashboard routes' do
-  it 'routes to #show' do
+  it 'routes to /dashboard' do
     expect(get: '/dashboard').to route_to(
       controller: 'dashboard', action: 'show')
   end
--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Home route' do
-  it 'routes to dashbard#show' do
+  it 'routes to /' do
     expect(get: '/').to route_to(
       controller: 'dashboard', action: 'show')
   end
