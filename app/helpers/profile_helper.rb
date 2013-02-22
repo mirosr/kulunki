5828bf734dfa67e2354a5697d9d5bc7b26449b3c
Miroslav Rachev <miro@mirosr.net>
2013-02-22 15:11:49 +0200

    Extract code into co_members_list helper

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,2 +1,9 @@
 module ProfileHelper
+  def co_members_list
+    if current_user.co_members.any?
+      current_user.co_members.map{ |m| m.username }.sort.join(', ')
+    else
+      'none'
+    end
+  end
 end
