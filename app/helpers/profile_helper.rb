d91614e99ecb9aa34f4580774137568924f5db57
Miroslav Rachev <miro@mirosr.net>
2013-02-22 17:51:02 +0200

    Extract code into household_name helper

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,4 +1,12 @@
 module ProfileHelper
+  def household_name
+    if current_user.household.present?
+      current_user.household.name
+    else
+      'none'
+    end
+  end
+
   def co_members_list
     if current_user.co_members.any?
       current_user.co_members.map{ |m| m.username }.sort.join(', ')
