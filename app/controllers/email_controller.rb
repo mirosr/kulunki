89489e471a5e8d6e4191029ba25c1d73766071d1
Miroslav Rachev <miro@mirosr.net>
2013-03-02 19:00:52 +0200

    Add initial EmailController

---
+++ app/controllers/email_controller.rb
@@ -0,0 +1,4 @@
+class EmailController < ApplicationController
+  def update
+  end
+end
---
+++ spec/controllers/email_controller_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe EmailController do
+  describe 'PUT #update' do
+  end
+end
