0927c4731ae57c85ab29caa0af16ff1a175e4643
Miroslav Rachev <miro@mirosr.net>
2013-03-12 23:22:03 +0200

    Move breadcrumb nav spec to header_spec.rb

--- spec/features/header_spec.rb
+++ spec/features/header_spec.rb
@@ -3,6 +3,18 @@ require 'spec_helper'
 feature 'Page Header' do
   include AuthHelper
 
+  scenario 'Showing the breadcrumb navigation' do
+    visit_protected root_path
+
+    expect(current_path).to eq(root_path)
+
+    within 'header.page .breadcrumb_nav' do
+      expect(page).to have_text 'Kulunki'
+      expect(page).to have_text 'Household Name'
+      expect(page).to have_text 'Current Period'
+    end
+  end
+
   scenario 'Show the link to user profile' do
     visit_protected_as root_path, username: 'john'
 
