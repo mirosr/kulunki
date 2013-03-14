7e0fed000f670fac09ff5e5622b7c6082924d40f
Miroslav Rachev <miro@mirosr.net>
2013-03-14 20:25:16 +0200

    Extract flash rendering into a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def render_flash_messages
+    divs = ''
+    flash.each do |type, value|
+      divs += content_tag(:div, value, class: type) if value.present?
+    end
+    divs.html_safe
+  end
 end
